18d3654eb0fd76f18d94479491484561f3d5d588
Jacob <jlbroughton88@gmail.com>
2025-04-09 22:27:25 -0400

    more constructor practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,31 @@
 import React from "react";
 import "./App.css";
 
-// export default function App() {
-//   const [goOut, setGoOut] = React.useState("Yes");
-
-//   function toggleGoOut() {
-//     setGoOut((prevState) => {
-//       return prevState === "Yes" ? "No" : "Yes";
-//     });
-//   }
-
-//   return (
-//     <div>
-//       <h1>Should I go out tonight?</h1>
-//       <div onClick={toggleGoOut}>
-//         <h1>{goOut}</h1>
-//       </div>
-//     </div>
-//   );
-// }
-
 export default class App extends React.Component {
   constructor() {
-    // calls constructor method of the parent class that we are extending from, gives us ability to use this.setState
     super();
     this.state = {
-      goOut: "Yes",
+      count: 0,
     };
 
-    // ensures the function exists within 'this'
-    this.toggleGoOut = this.toggleGoOut.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
   }
 
-  // class method
-  // use arrow function if it uses this.setstate
-  toggleGoOut = () => {
-    this.setState((prevState) => {
-      return {
-        goOut: prevState.goOut === "Yes" ? "No" : "Yes",
-      };
-    });
-  };
+  add() {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  }
+
+  subtract() {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  }
 
   render() {
     return (
       <div>
-        <h1>Should I go out tonight?</h1>
-        <div onClick={this.toggleGoOut}>
-          <h1>{this.state.goOut}</h1>
-        </div>
+        <button onClick={this.subtract}>-</button>
+        {this.state.count}
+        <button onClick={this.add}>+</button>
       </div>
     );
   }
